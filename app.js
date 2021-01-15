a7b20011e7f648896a4fba612826b440979e7005
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-01-14 23:29:17 -0600

    app changed

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world from SaiKrishnas app!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Heroku app')
+  })
+
+  app.get('/contact', (req, res) => {
+    res.send('s538340@nwmissouri.edu')
+  })
+
+  app.get('/help', (req, res) => {
+    res.send('contact me for any help')
+  })
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
